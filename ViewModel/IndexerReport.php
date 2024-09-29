8ef046120c5e6aceb65e3a80f0657e6c9b24cbfe
Kiel Pykett <pykettk@gmail.com>
2024-09-29 21:24:17 +0100

    Create Locked Modes Notice Section

--- ViewModel/IndexerReport.php
+++ ViewModel/IndexerReport.php
@@ -8,17 +8,24 @@ declare(strict_types=1);
 namespace Element119\AdminIndexerReport\ViewModel;
 
 use Element119\AdminIndexerReport\Model\IndexerInfo;
+use Magento\Framework\Module\ModuleListInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class IndexerReport implements ArgumentInterface
 {
     public function __construct(
         private readonly IndexerInfo $indexerInfo,
+        private readonly ModuleListInterface $moduleList,
     ) {
     }
 
     public function getIndexerInfo(): IndexerInfo
     {
         return $this->indexerInfo;
+    }
+
+    public function isIndexerDeployConfigModuleInstalled(): bool
+    {
+        return $this->moduleList->has('Element119_IndexerDeployConfig');
     }
 }
